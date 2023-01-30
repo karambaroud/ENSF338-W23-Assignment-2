f4104bb97041bae6ce00ff71b57d5c54dd41a450
karambaroud <113051905+karambaroud@users.noreply.github.com>
2023-01-30 15:22:06 -0700

    Added a main() function and if _name_ == '_main_'

--- ex1.5.py
+++ ex1.5.py
@@ -19,34 +19,38 @@ def func2(n):
         cache[n] = func2(n-1) + func2(n-2)
         return cache[n]
 
-#Empirical analysis of the original function.
-func_time_list = []
-for i in range(0, 36):
-    elapsed_time = timeit.timeit(lambda : func(i), number=1)
-    func_time_list.append(elapsed_time)
+def main():
+    #Empirical analysis of the original function.
+    func_time_list = []
+    for i in range(0, 36):
+        elapsed_time = timeit.timeit(lambda : func(i), number=1)
+        func_time_list.append(elapsed_time)
 
-#Empirical analysis of the improved function.
-func2_time_list = []
-for i in range(0, 36):
-    elapsed_time = timeit.timeit(lambda : func2(i), number=1)
-    func2_time_list.append(elapsed_time)
+    #Empirical analysis of the improved function.
+    func2_time_list = []
+    for i in range(0, 36):
+        elapsed_time = timeit.timeit(lambda : func2(i), number=1)
+        func2_time_list.append(elapsed_time)
 
-#Plotting the results
-fig, axs = plt.subplots(figsize = (10, 5))
+    #Plotting the results
+    fig, axs = plt.subplots(figsize = (10, 5))
 
-axs1 = plt.subplot2grid ( shape = (1,2), loc = (0,0))
-axs2 = plt.subplot2grid ( shape = (1,2), loc = (0,1))
-plt.tight_layout()
+    axs1 = plt.subplot2grid ( shape = (1,2), loc = (0,0))
+    axs2 = plt.subplot2grid ( shape = (1,2), loc = (0,1))
+    plt.tight_layout()
 
-axs1.plot(func_time_list, label='original')
-axs1.legend(loc='upper left')
-axs1.set_xlabel('Argument')
-axs1.set_ylabel('Time (s)')
+    axs1.plot(func_time_list, label='original')
+    axs1.legend(loc='upper left')
+    axs1.set_xlabel('Argument')
+    axs1.set_ylabel('Time (s)')
 
-axs2.plot(func2_time_list, color="r", label='improved')
-axs2.legend(loc='upper left')
-axs2.set_xlabel('Argument')
-axs2.set_ylabel('Time (s)')
+    axs2.plot(func2_time_list, color="r", label='improved')
+    axs2.legend(loc='upper left')
+    axs2.set_xlabel('Argument')
+    axs2.set_ylabel('Time (s)')
 
-plt.show()
+    plt.show()
+
+if __name__ == '__main__':
+    main()
 
